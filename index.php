dfd294059e5e7bada117371c14b42aa1bf88fcf6
Giselle Cristina <gisellepereira1207@gmail.com>
2024-06-05 00:55:24 -0300

    first-commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Cadastro</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Cadastrar</h1>
+        <form action="" class="form" >
+            <label for="name">Nome:</label>
+            <input type="text" name="name">
+            <label for="name">Email:</label>
+            <input type="text" name="email">
+            <label for="name">Data de Nascimento:</label>
+            <input type="text" name="date">
+            <label for="name">Telefone:</label>
+            <input type="text" name="tel">
+            <label for="name">Senha:</label>
+            <input type="text" name="password">
+            <label for="name">Confirmar senha:</label>
+            <input type="text" name="confirm_password">
+            <input type="text" name="submit" value="Enviar" id="btn">
+    </div>
+    </form>
+</body>
+</html>
