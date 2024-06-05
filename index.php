21a8ca2f4d4215a762bddad08260047bfd0a15a6
Giselle Cristina <gisellepereira1207@gmail.com>
2024-06-05 01:43:48 -0300

    put img

--- index.php
+++ index.php
@@ -1,29 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Cadastro</title>
 </head>
+
 <body>
     <div class="container">
-        <h1>Cadastrar</h1>
-        <form action="" class="form" >
-            <label for="name">Nome:</label>
-            <input type="text" name="name">
-            <label for="name">Email:</label>
-            <input type="text" name="email">
-            <label for="name">Data de Nascimento:</label>
-            <input type="text" name="date">
-            <label for="name">Telefone:</label>
-            <input type="text" name="tel">
-            <label for="name">Senha:</label>
-            <input type="text" name="password">
-            <label for="name">Confirmar senha:</label>
-            <input type="text" name="confirm_password">
-            <input type="text" name="submit" value="Enviar" id="btn">
+        <div class="form_content">
+            <h1>Cadastrar</h1>
+            <form action="" class="form">
+                <label for="name">Nome:</label>
+                <input type="text" name="name">
+                <label for="name">Email:</label>
+                <input type="text" name="email">
+                <label for="name">Data de Nascimento:</label>
+                <input type="text" name="date">
+                <label for="name">Telefone:</label>
+                <input type="text" name="tel">
+                <label for="name">Senha:</label>
+                <input type="text" name="password">
+                <label for="name">Confirmar senha:</label>
+                <input type="text" name="confirm_password">
+                <input type="text" name="submit" value="Enviar" id="btn">
+            </form>
+        </div>
+        <div class="img">
+
+        </div>
     </div>
-    </form>
 </body>
+
 </html>
